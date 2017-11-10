8a5921469889979f21d55552f72e3a7a52137804
Maxime <praxus@laposte.net>
2017-11-10 14:58:40 +0100

    operation par niveau operationelle - ajout de la selection de niveaux

--- app/src/main/java/com/example/cassa/entrainementprojettut/MainActivity.java
+++ app/src/main/java/com/example/cassa/entrainementprojettut/MainActivity.java
@@ -34,7 +34,7 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
 
                 player.stop();
-                Intent additionIntent = new Intent(MainActivity.this, AdditionActivity.class);
+                Intent additionIntent = new Intent(MainActivity.this, choixDifficulteActivity.class);
                 startActivity(additionIntent);
 
 
