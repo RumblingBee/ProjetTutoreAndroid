7333bd368795fbe9075e19eef50e3c372c366825
Clement <clement.monestier@hotmail.fr>
2017-12-07 13:20:32 +0100

    Création layout geographyTag

---
+++ app/src/main/java/com/example/cassa/entrainementprojettut/GeographyTag.java
@@ -0,0 +1,17 @@
+package com.example.cassa.entrainementprojettut;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+/**
+ * Created by clement on 07/12/17.
+ */
+
+public class GeographyTag extends GameActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_geographytag);
+    }
+}
