d8d2d6edc5fbda8cbc9793e651e841bf8e643620
Chapuisat <vchapuisat@orange.fr>
2018-01-02 17:33:28 +0100

    refactoring de flag, flagBank et fagActivity

--- app/src/main/java/com/example/cassa/entrainementprojettut/FlagActivity.java
+++ app/src/main/java/com/example/cassa/entrainementprojettut/FlagActivity.java
@@ -31,39 +31,20 @@ public class FlagActivity extends GameActivity implements View.OnClickListener {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_flag);
-
         lancerBgMusique(FlagActivity.this, R.raw.bensound_funnysong);
-        //  lancerCourse(FlagActivity.this, R.drawable.);
-
-        mDrapeau1 = (ImageView)findViewById(R.id.activity_flag_flag01);
-        mDrapeau2 = (ImageView)findViewById(R.id.activity_flag_flag02);
-        mDrapeau3 = (ImageView)findViewById(R.id.activity_flag_flag03);
-        mDrapeau4 = (ImageView)findViewById(R.id.activity_flag_flag04);
-
-        mDrapeau1.setOnClickListener(this);
-        mDrapeau2.setOnClickListener(this);
-        mDrapeau3.setOnClickListener(this);
-        mDrapeau4.setOnClickListener(this);
-
-        mNomPays = (TextView)findViewById(R.id.activity_flag_name01_txt);
-        mScore = (TextView)findViewById(R.id.activity_flag_score_txt);
 
         diff = getIntent().getIntExtra("diff", 1);
 
-        // On initialise le score à 0
-        gScore = 0;
-        gNbBonneReponse = 0;
+        initialisationDrapeaux();
 
-        mScore.setText("0");
+        initialisationNomPaysEtScore();
 
         afficherChoixNiveaux(FlagActivity.this,"listeNiveau",3);
         dialog.setOnDismissListener(new DialogInterface.OnDismissListener() {
             @Override
             public void onDismiss(DialogInterface dialogInterface) {
                 if (niveauChoisi != 0) {
-
                     genererPartie();
-
                 } else {
                     FlagActivity.this.onStop();
                     dialog.show();
@@ -74,15 +55,72 @@ public class FlagActivity extends GameActivity implements View.OnClickListener {
 
 
     }
+
+
+
+    private void initialisationNomPaysEtScore() {
+        mNomPays = (TextView)findViewById(R.id.activity_flag_name01_txt);
+        mScore = (TextView)findViewById(R.id.activity_flag_score_txt);
+
+        // On initialise le score à 0
+        gScore = 0;
+        gNbBonneReponse = 0;
+        mScore.setText("0");
+    }
+
+
+
+    private void initialisationDrapeaux() {
+        mDrapeau1 = (ImageView)findViewById(R.id.activity_flag_flag01);
+        mDrapeau2 = (ImageView)findViewById(R.id.activity_flag_flag02);
+        mDrapeau3 = (ImageView)findViewById(R.id.activity_flag_flag03);
+        mDrapeau4 = (ImageView)findViewById(R.id.activity_flag_flag04);
+
+        mDrapeau1.setOnClickListener(this);
+        mDrapeau2.setOnClickListener(this);
+        mDrapeau3.setOnClickListener(this);
+        mDrapeau4.setOnClickListener(this);
+    }
+
+
+
     protected void genererPartie(){
 
         FlagBank flagBank = new FlagBank(niveauChoisi);
-
-
         ImageView listeDrapeau[] = {mDrapeau1,mDrapeau2,mDrapeau3,mDrapeau4};
 
-//On réactive tous les drapeaux
+        //On réactive tous les drapeaux
+        reactivationDrapeaux();
+        //On génère le nom du pays à trouver
+        generationPaysATrouver(flagBank);
 
+        //On génère les drapeaux
+        generationChoixDrapeaux(flagBank, listeDrapeau);
+
+    }
+
+
+
+    private void generationChoixDrapeaux(FlagBank flagBank, ImageView[] listeDrapeau) {
+        for(int i=0; i <4;i++) {
+            listeDrapeau[i].setImageResource(flagBank.getFlag(i).getmRessource());
+            listeDrapeau[i].setTag(flagBank.getFlag(i).getmNameCountry());
+        }
+    }
+
+
+
+    private void generationPaysATrouver(FlagBank flagBank) {
+        Random rand = new Random();
+        int numPaysMystere = rand.nextInt(4);
+
+        mNomPays.setText(flagBank.getFlag(numPaysMystere).getmNameCountry());
+        gBonneReponse = flagBank.getFlag(numPaysMystere).getmNameCountry();
+    }
+
+
+
+    private void reactivationDrapeaux() {
         mDrapeau1.setEnabled(true);
         mDrapeau2.setEnabled(true);
         mDrapeau3.setEnabled(true);
@@ -92,50 +130,36 @@ public class FlagActivity extends GameActivity implements View.OnClickListener {
         mDrapeau2.setColorFilter(0);
         mDrapeau3.setColorFilter(0);
         mDrapeau4.setColorFilter(0);
+    }
 
-//On génère le nom du pays
-        Random rand = new Random();
-        int numPaysMystere = rand.nextInt(4);
 
-        mNomPays.setText(flagBank.getFlag(numPaysMystere).getmNameCountry());
-        gBonneReponse = flagBank.getFlag(numPaysMystere).getmNameCountry();
 
-//On génère les drapeaux
+    protected  void verifierReponse(ImageView v,String pPays){
 
-        for(int i=0; i <4;i++) {
-            listeDrapeau[i].setImageResource(flagBank.getFlag(i).getmRessource());
-            listeDrapeau[i].setTag(flagBank.getFlag(i).getmNameCountry());
-
+        if(pPays == gBonneReponse ){
+            afficherTexte("Bravo");
+            gScore = gScore + 5;
+            gNbBonneReponse = gNbBonneReponse + 1;
+            mScore.setText(""+gScore);
+            genererPartie();
+            if(gNbBonneReponse == 10 ){
+                afficherEcranFin(FlagActivity.this,true,true,gScore);
+            }
+        }
+        else{
+            afficherTexte("Dommage");
+            gScore = gScore - 2;
+            v.setBackgroundColor(Color.argb(150,200,200,200));
+            v.setEnabled(false);
+            mScore.setText(""+gScore);
+            v.setColorFilter(R.color.material_grey_600);
         }
 
     }
-protected  void verifierReponse(ImageView v,String pPays){
-
-    if(pPays == gBonneReponse ){
-        afficherTexte("Bravo");
-        gScore = gScore + 5;
-        gNbBonneReponse = gNbBonneReponse + 1;
-        mScore.setText(""+gScore);
-        genererPartie();
-        if(gNbBonneReponse == 10 ){
-           afficherEcranFin(FlagActivity.this,true,true,gScore);
-        }
-    }
-    else{
-        afficherTexte("Dommage");
-        gScore = gScore - 2;
-        v.setBackgroundColor(Color.argb(150,200,200,200));
-        v.setEnabled(false);
-        mScore.setText(""+gScore);
-        v.setColorFilter(R.color.material_grey_600);
-    }
-
-}
 
 
     @Override
     public void onClick(View view) {
-
         String paysSelectione = (String) view.getTag();
 
         verifierReponse((ImageView) view,paysSelectione);
--- app/src/main/java/com/example/cassa/entrainementprojettut/FlagBank.java
+++ app/src/main/java/com/example/cassa/entrainementprojettut/FlagBank.java
@@ -1,7 +1,5 @@
 package com.example.cassa.entrainementprojettut;
 
-import android.widget.ImageView;
-
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Random;
@@ -11,12 +9,12 @@ import java.util.Random;
  */
 
 public class FlagBank {
-    ArrayList<Flag> mListFlag;
+    ArrayList<Flag> mListChoixFlag;
     ArrayList<Integer> mListAdresseImage;
     ArrayList<String> mListNomPays;
     private static String[] mPays = {"France", "Allemagne", "Italie", "Espagne", "Pays-Bas", "Portugal", "Suisse", "Royaume-Uni", "Belgique", "Russie",
-                                    "Canada", "Etats-Unis", "Brésil", "Chine", "Australie", "Afrique Du Sud", "Japon", "Argentine", "Algérie", "Mexique",
-                                    "Autriche", "Maroc", "Tunisie", "Turquie", "Nouvelle-Zélande", "Inde", "Pérou", "Corée Du Sud", "Egypte", "Chili"};
+            "Canada", "Etats-Unis", "Brésil", "Chine", "Australie", "Afrique Du Sud", "Japon", "Argentine", "Algérie", "Mexique",
+            "Autriche", "Maroc", "Tunisie", "Turquie", "Nouvelle-Zélande", "Inde", "Pérou", "Corée Du Sud", "Egypte", "Chili"};
 
 
 
@@ -28,116 +26,119 @@ public class FlagBank {
     //Rand de séléction de pays
     private Random rand = new Random();
 
+
+
     public FlagBank(ArrayList<Flag> listFlag){
-        mListFlag = listFlag;
+        if(listFlag != null && listFlag.size() == 4) {
+            mListChoixFlag = listFlag;
+        }else{
+            initialisationImagesPays();
+            initialisationNomPays(1);
+            initialisationChoixFlag(1);
+        }
     }
 
-   public FlagBank(int diff){
+
+
+    public FlagBank(int diff){
+
+        mListChoixFlag = new ArrayList<Flag>();
+        mListNomPays = new ArrayList<String>();
+        mListAdresseImage = new ArrayList<Integer>();
+        mListChoixFlag = new ArrayList<Flag>(4);
+
+        initialisationImagesPays();
+        initialisationNomPays(diff);
+        initialisationChoixFlag(diff);
+    }
 
 
 
-       mListFlag = new ArrayList<Flag>();
-       mListNomPays = new ArrayList<String>();
-       mListAdresseImage = new ArrayList<Integer>();
+    private void initialisationChoixFlag(int diff) {
+        //ArrayList pour éviter les doublons
+        ArrayList<Integer> dispo = new ArrayList<Integer>();
+        for(int i=0; i <4;i++){
+            int x=0;
+            //J'ai essayé avec un switch ça c'est pas très bien passé
+            //Je sais pas pourquoi il passait dans tous les cas
+            if(diff == 1){
+                x = rand.nextInt(10);
+            }else if(diff == 2){
+                x = rand.nextInt(20);
+            }else if(diff == 3){
+                x = rand.nextInt(30);
+            }
 
-       //Génération des images drapeaux
+            mFLag = new Flag(mListNomPays.get(x),mListAdresseImage.get(x));
+            //Pour éviter d'ajouter deux fois le même Flag
+            if(dispo.contains(x)) {
+                i--;
+            }else {
+                mListChoixFlag.add(mFLag);
+                dispo.add(x);
+            }
+        }
 
-       //Pays faciles
-       mListAdresseImage.add(R.drawable.france);
-       mListAdresseImage.add(R.drawable.allemagne);
-       mListAdresseImage.add(R.drawable.italie);
-       mListAdresseImage.add(R.drawable.espagne);
-       mListAdresseImage.add(R.drawable.pays_bas);
-       mListAdresseImage.add(R.drawable.portugal);
-       mListAdresseImage.add(R.drawable.suisse);
-       mListAdresseImage.add(R.drawable.royaume_uni);
-       mListAdresseImage.add(R.drawable.belgique);
-       mListAdresseImage.add(R.drawable.russie);
+        Collections.shuffle(mListChoixFlag);
+    }
 
-       //Pays moins facile
-       mListAdresseImage.add(R.drawable.canada);
-       mListAdresseImage.add(R.drawable.etats_unis);
-       mListAdresseImage.add(R.drawable.bresil);
-       mListAdresseImage.add(R.drawable.chine);
-       mListAdresseImage.add(R.drawable.australie);
-       mListAdresseImage.add(R.drawable.afrique_du_sud);
-       mListAdresseImage.add(R.drawable.japon);
-       mListAdresseImage.add(R.drawable.argentine);
-       mListAdresseImage.add(R.drawable.algerie);
-       mListAdresseImage.add(R.drawable.mexique);
-
-       //Pays difficiles
-       mListAdresseImage.add(R.drawable.autriche);
-       mListAdresseImage.add(R.drawable.maroc);
-       mListAdresseImage.add(R.drawable.tunisie);
-       mListAdresseImage.add(R.drawable.turquie);
-       mListAdresseImage.add(R.drawable.nouvelle_zelande);
-       mListAdresseImage.add(R.drawable.inde);
-       mListAdresseImage.add(R.drawable.perou);
-       mListAdresseImage.add(R.drawable.coree_cu_sud);
-       mListAdresseImage.add(R.drawable.egypte);
-       mListAdresseImage.add(R.drawable.chili);
-
-
-
-
-
-       switch (diff){
-           case 1:
-               for(int i=0; i<10; i++){
-                   mListNomPays.add(mPays[i]);
-               }
-           case 2:
-               for (int i=0; i<20; i++){
-                   mListNomPays.add(mPays[i]);
-               }
-           case 3:
-               for (int i=0; i<30; i++){
-                   mListNomPays.add(mPays[i]);
-               }
-       }
+    private void initialisationNomPays(int diff) {
+        switch (diff){
+            case 1:
+                for(int i=0; i<10; i++){
+                    mListNomPays.add(mPays[i]);
+                }
+            case 2:
+                for (int i=0; i<20; i++){
+                    mListNomPays.add(mPays[i]);
+                }
+            case 3:
+                for (int i=0; i<30; i++){
+                    mListNomPays.add(mPays[i]);
+                }
+        }
+    }
 
 
+    private void initialisationImagesPays() {
+        //Pays faciles
+        mListAdresseImage.add(R.drawable.france);
+        mListAdresseImage.add(R.drawable.allemagne);
+        mListAdresseImage.add(R.drawable.italie);
+        mListAdresseImage.add(R.drawable.espagne);
+        mListAdresseImage.add(R.drawable.pays_bas);
+        mListAdresseImage.add(R.drawable.portugal);
+        mListAdresseImage.add(R.drawable.suisse);
+        mListAdresseImage.add(R.drawable.royaume_uni);
+        mListAdresseImage.add(R.drawable.belgique);
+        mListAdresseImage.add(R.drawable.russie);
+
+        //Pays moins facile
+        mListAdresseImage.add(R.drawable.canada);
+        mListAdresseImage.add(R.drawable.etats_unis);
+        mListAdresseImage.add(R.drawable.bresil);
+        mListAdresseImage.add(R.drawable.chine);
+        mListAdresseImage.add(R.drawable.australie);
+        mListAdresseImage.add(R.drawable.afrique_du_sud);
+        mListAdresseImage.add(R.drawable.japon);
+        mListAdresseImage.add(R.drawable.argentine);
+        mListAdresseImage.add(R.drawable.algerie);
+        mListAdresseImage.add(R.drawable.mexique);
+
+        //Pays difficiles
+        mListAdresseImage.add(R.drawable.autriche);
+        mListAdresseImage.add(R.drawable.maroc);
+        mListAdresseImage.add(R.drawable.tunisie);
+        mListAdresseImage.add(R.drawable.turquie);
+        mListAdresseImage.add(R.drawable.nouvelle_zelande);
+        mListAdresseImage.add(R.drawable.inde);
+        mListAdresseImage.add(R.drawable.perou);
+        mListAdresseImage.add(R.drawable.coree_cu_sud);
+        mListAdresseImage.add(R.drawable.egypte);
+        mListAdresseImage.add(R.drawable.chili);
+    }
 
 
-       // Générations des réponses FLags
-       mListFlag = new ArrayList<Flag>(4);
-
-       //ArrayList pour éviter les doublons
-       ArrayList<Integer> dispo = new ArrayList<Integer>();
-
-       for(int i=0; i <4;i++){
-           int x=0;
-           //J'ai essayé avec un switch ça c'est pas très bien passé
-           //Je sais pas pourquoi il passait dans tous les cas
-           if(diff == 1){
-               x = rand.nextInt(10);
-           }else if(diff == 2){
-               x = rand.nextInt(20);
-           }else if(diff == 3){
-               x = rand.nextInt(30);
-           }
-
-           mFLag = new Flag(mListNomPays.get(x),mListAdresseImage.get(x));
-           //Pour éviter d'ajouter deux fois le même Flag
-           if(dispo.contains(x)) {
-               i--;
-           }else {
-               mListFlag.add(mFLag);
-               dispo.add(x);
-           }
-
-       }
-
-       Collections.shuffle(mListFlag);
-
-
-
-   }
-  Flag getFlag(int i){
-
-       return  mListFlag.get(i);
-
-   }
+    Flag getFlag(int i){ return  mListChoixFlag.get(i); }
 
 }
