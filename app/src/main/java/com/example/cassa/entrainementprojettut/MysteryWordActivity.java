1f609e640f4e429d687254b3f08acfea206ff886
Clement <clement.monestier@hotmail.fr>
2018-01-09 10:13:18 +0100

    refactor setOperation Ajout d'une facotry

--- app/src/main/java/com/example/cassa/entrainementprojettut/MysteryWordActivity.java
+++ app/src/main/java/com/example/cassa/entrainementprojettut/MysteryWordActivity.java
@@ -8,7 +8,6 @@ import android.os.Handler;
 import android.support.annotation.Nullable;
 import android.view.View;
 import android.view.WindowManager;
-import android.view.animation.TranslateAnimation;
 import android.widget.Button;
 import android.widget.ToggleButton;
 import android.widget.ImageView;
