90358f8dc58bfcba5ae64c9acd654460fe8474e6
Hugbald <clement.monestier@hotmail.fr>
2018-01-16 16:29:55 +0100

    Implémentation Music et Test

--- app/src/main/java/com/example/cassa/entrainementprojettut/pianoGame/Music.java
+++ app/src/main/java/com/example/cassa/entrainementprojettut/pianoGame/Music.java
@@ -1,17 +1,36 @@
 package com.example.cassa.entrainementprojettut.pianoGame;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
  * Created by clement on 16/01/18.
  */
 
-public abstract class Music {
+public class Music {
 
     private List<Note> notes;
+    private List<Note> sequence;
+    private int position;
 
-    public List<Note> generateSequence(int i){
-        
+    public Music(List<Note> notes) {
+        this.notes = notes;
+        this.sequence=new ArrayList<>();
     }
 
+    public List<Note> generateSequence(int size){
+        for (int j = 0; j <size ; j++) {
+            sequence.add(notes.get(j));
+        }
+        position=size;
+        return sequence;
+    }
+
+    public List<Note> incrementSequence(int i) {
+        for (int j = position; j <position+i ; j++) {
+            sequence.add(notes.get(j));
+        }
+        position+=i;
+        return sequence;
+    }
 }
